ba3c5de921f82b5d2c087674fe1659f4d37d3622
Christine <christine_chapman@brown.edu>
2014-03-25 14:37:56 -0400

    set up dining hall initial call.  Still getting Access-Control-Allow-Origin issues though

--- home.js
+++ home.js
@@ -2,9 +2,7 @@ $(document).ready(function(){
 	$('a').each(function(){
 		$(this).click(function(event){
 			event.preventDefault();
-			var request = new XMLHttpRequest();	
-			request.open('GET', '/' + $(this).attr('id'));
-			request.send();
+			$.get('/' + $(this).attr('id'));
 		});	
 	});
 }); 
--- server.js
+++ server.js
@@ -7,20 +7,6 @@ app.use(express.static(__dirname)); //allows css to work with rendered html
 
 app.use(express.bodyParser());
  
-app.post('*', function(req, res){
-	conn.query('SELECT * FROM zipcodes WHERE zipcode="' + req.body.zipcode +'"', function(error, result) {
-		if(error !== null){
-			res.end(error);
-		} else if(result.rows.length === 0){
-			res.end("<html><body>Invalid Zipcode!" + mainPage + "</body></html>");
-		} else {
-			res.end("<html><body>Zipcode: " + req.body.zipcode + " is located in " + result.rows[0].city + ", " + result.rows[0].state + "<br>Again?" + mainPage + "</body></html>")
-		}
-	});
-
-
-});
-
 //Navigates to Brown's get portal to get credit/points info
 app.get('/login', function(req, res){
 	res.header('Access-Control-Allow-Origin', '*');
@@ -28,8 +14,9 @@ app.get('/login', function(req, res){
 	res.header('Access-Control-Allow-headers','Content-Type,Authorization,Content-Length,X-Request-With');
 	res.redirect('http://www.brown.edu/getportal');
 });
-
-
+app.get('/dininghalls', function(req, res) {
+	res.redirect('http://www.brown.edu/Student_Services/Food_Services/eateries/refectory.php');
+});
 app.get('/', function(req, res){
 	res.render('home.html');
 });
