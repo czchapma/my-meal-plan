0f3a805f37228518bfd28ee49d128666f6cfecae
Steven <steven@ubuntu.(none)>
2014-03-28 14:41:26 -0400

    Changed data structure reprsentation of KNN, wrote untested add code.

--- ML_Client.java
+++ ML_Client.java
@@ -1,24 +1,24 @@
 import java.util.*;
 public class ML_Client
 {
-	private ArrayList<User_Reviews> users; // N users maintained sorted by id number ascending (0,1,2,3)
-	private ArrayList<ArrayList<Double>> dists; //an N by N matrix, where (i,j) is the distance between users i and j, according to the dist function of User_Reviews. Inherently should be symmetric with zeros on the diagonal. 
+	private HashMap<Integer, User_Reviews> users; // N users pointed to by their userId
+	private HashMap<Integer, HashMap<Integer,Double>> dists; //distances hashmap. So the hashmap provides a hashmap of distances. as in user number 3 points to a map wherein each other user will then point to a double. essentially a matrix, but done this way in case id numbers arent sequential
 
 	//eventually add a weights vector for age, gender etc.
 	//also weights for each food (doing KNN right now, so don't need weights, but it makes sense that how much two people like milk is less important than say, if two people like a very random, specific flavor of yogurt)
 
 	public ML_Client()
 	{
-		users = new ArrayList<User_Reviews>();
-		dists = new ArrayList<ArrayList<Double>>();
+		users = new HashMap<Integer,User_Reviews>();
+		dists = new HashMap<Integer, HashMap<Integer,Double>>();
 	}
 
 	//TODO: make a constructor that can open a saved users array and a saved dists matrix
 	public ML_Client(String fileNameUsers, String fileNameDists)
 	{
 		//TODO: fetch actual data from fileNameUsers, and fileNameDists
-		users = new ArrayList<User_Reviews>();
-		dists = new ArrayList<ArrayList<Double>>();
+		users = new HashMap<Integer,User_Reviews>();
+		dists = new HashMap<Integer, HashMap<Integer,Double>>();
 	}
 
 	//TODO: make a method that will save the current state of users and dists somehow to the given locations
@@ -29,10 +29,28 @@ public class ML_Client
 		return false;
 	}
 
-	//TODO: add newUser into users, maintaining the sort by id number ascending. If id number already exists, should instead call updateUser, and issue a warning that the user already existed. 
-	//Then update dists so that there is a new column and row for that user. 
+	//TODO: Test that the method does the following: add newUser into users. If id number already exists, should issue a warning that the user already existed. Then update dists so that there is a new "column and row" for that user. 
 	public void addUser(User_Reviews newUser)
 	{
+		if(users.containsKey(newUser.getUserId()))
+		{
+			System.out.println("Warning: users already contains this userId. Aborting.");
+			return;
+		}
+		else
+		{
+			users.put(newUser.getUserId(),newUser); //add the new user to the map of users.
+			HashMap<Integer, Double> newDist = new HashMap<Integer,Double>();
+
+			for(User_Reviews user : users.values()) //now go through each user
+			{
+				newDist.put(user.getUserId(), user.diff(newUser)); //add their dist to newUser to the newDist
+				dists.get(user.getUserId()).put(newUser.getUserId(),user.diff(newUser)); //and add the dist to the other user's dists.
+			}
+			dists.put(newUser.getUserId(),newDist);//and add the newDist into dists.
+				
+		}
+			
 	}
 
 	//TODO: should go to the user in users indicated by the curId (j) and update the name
@@ -81,7 +99,11 @@ public class ML_Client
 		String[] suggestions = new String[numWanted];
 		return suggestions;
 	}
-		
+	
+	//TODO: We really really need a toString for testing. Should print the contents of both data structures in a legible way.
+	public String toString()
+	{
+	}	
 
 }
 		
