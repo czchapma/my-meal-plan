415ce3e985d0f0b0dacb4ffe54d975c2a20bfca3
Raymond Zeng <raymondzeng@live.com>
2014-04-13 20:15:36 -0400

    added html/css for browse items

--- mydininglog.js
+++ mydininglog.js
@@ -7,10 +7,18 @@ $(document).ready(function(){
     $('#allpurchase').click(function() {
 	$('#prev-transactions').show();
 	$('#log-form').hide();
+	$('#browse-items').hide();
     });
     
     $('#purchase').click(function() {
 	$('#log-form').show();
+	$('#prev-transactions').hide();
+	$('#browse-items').hide();
+    });
+    
+    $('#browse').click(function() {
+	$('#browse-items').show();
+	$('#log-form').hide();
 	$('#prev-transactions').hide();
     });
 });
