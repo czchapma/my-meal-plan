32123a5f9d72f76ab10defc134cf253b47be3e06
Steven <steven_mcgarty@brown.edu>
2014-04-26 20:56:08 -0400

    Changed display of previous transactions

--- static/js/prevtransactions.js
+++ static/js/prevtransactions.js
@@ -4,10 +4,51 @@ $(document).ready(function(){
     }).done(function(result) {
         var ul = $('#prev-transactions ul');
         ul.html('');
+        curchunk = 0;
+        prevdates = [];
+        prevul = null;
+       /*result = [{item:"Item1", date:"Date1"},
+                   {item:"Item2", date:"Date1"},
+                {item:"Item1", date:"Date2"},
+                {item:"Item1", date:"Date3"},
+                {item:"Item3", date:"Date1"},
+                {item:"Item3", date:"Date2"},
+                {item:"Item2", date:"Date2"},
+                {item:"Item3", date:"Date1"},
+                {item:"Item2", date:"Date3"},
+                {item:"Item3", date:"Date3"},
+                {item:"Item4", date:"Date3"},
+                {item:"Item4", date:"Date1"},
+                {item:"Item5", date:"Date1"},
+                {item:"Item4", date:"Date2"},
+                {item:"Item5", date:"Date2"}];*/ //test data
         for (var i=0; i<result.length; i++){
             var json = JSON.parse(JSON.stringify(result[i]));
+
+            var item = json['item'];
+            var date = json['date'];
             var li = '<li><div class="prev-trans-item">'+ json['item'] + '</div><div class="prev-trans-date">' + json['date'] + '</div></li>';
-            ul.append(li);
+            if(prevdates.indexOf(date) !== -1)
+            {
+                console.log(item);
+                var datenospaces = date;
+                while(datenospaces.indexOf(" ") !== -1)
+                     datenospaces = datenospaces.replace(" ","");
+                $("#" + datenospaces).append(li);
+            }
+            else
+            {
+                newul = $(document.createElement('ul'));
+                var datenospaces = date;
+                while(datenospaces.indexOf(" ") !== -1)
+                     datenospaces = datenospaces.replace(" ","");
+                newul.attr('id',datenospaces);
+                prevdates.push(date);
+                newul.append("<h1>" + date + "</h1>");
+                newul.append(li);
+                ul.append(newul); 
+            }
+            
         }
 
         if(result.length === 0){
