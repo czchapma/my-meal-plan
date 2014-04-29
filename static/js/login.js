3f3c413099b7194ec36bf54cddc0e5c940c7e6d7
Christine <christine_chapman@brown.edu>
2014-04-29 01:12:22 -0400

    [redacted] super secret

--- static/js/login.js
+++ static/js/login.js
@@ -13,4 +13,16 @@ $(document).ready(function(){
             $('#welcomeMsg').text('');
     	}
     });
+
+    //secret thing
+    var rand = Math.floor((Math.random()*10000)+1);
+    if (rand === 2222){
+        $('#nav_bar').css({'background-color':'#7fedf0'});
+        $('body').css({'background-color':'#C6F2FC'});
+        $('#home-content').css({'background-color':'#FFF6DB'});
+        $('#nav_bar').append('<audio id="letitgo" src="letitgo.mp3" preload="auto"></audio>');   
+        $('#nav_buttons a:hover').css({'background-color':'#FFF6DB'})     
+        document.getElementById('letitgo').play();
+        $('img').attr('src','static/imgs/frozen.jpg');
+    }
 });
