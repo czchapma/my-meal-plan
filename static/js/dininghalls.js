00c7372d1f321276bce7b6133b46f66bf858dfe8
zolstein <zachary_olstein@brown.edu>
2014-05-03 21:22:42 -0400

    Updated to organize Ratty menu items by lane

--- static/js/dininghalls.js
+++ static/js/dininghalls.js
@@ -167,6 +167,7 @@ function generateMenus(){
 }
 function generateRatty(){
     var meals = ['breakfast','lunch','dinner'];
+    var lines = ['Roots & Shoots', 'Chef\'s Corner', 'Bistro', 'Grill', 'Specials'];
     for (var mealId=0; mealId<meals.length; mealId++){
     	$.post( "/menu/ratty", {meal:meals[mealId]}, function(data,status){
     		if (data === ''){
@@ -175,10 +176,28 @@ function generateRatty(){
 			}
     		var split = data.split('\n');
     		var meal = split[0];
-			var parent = $('#tab-content1 #ratty_' + meal);
-			for (var i=1; i<split.length; i++){
-			    var li ='<li>'+ split[i] + '</li>';
-			    parent.append(li);
+			var parents = $('#tab-content1 #ratty_' + meal).children('ul');
+			var parentIdx = 0;
+            var parent;
+            var numInLine = 0;
+            for (var i=1; i<split.length; i++){
+                var text = split[i];
+                if (lines.indexOf(text) !== -1){
+                    //Add blank element to make even number of elements
+                    //Fixes weird CSS glitch
+                    if (numInLine % 2 !== 0){
+                        var li = document.createElement('li');
+                        li.innerHTML = "&nbsp;";
+                        parent.appendChild(li);
+                    }
+                    parent = parents[parentIdx++];
+                    numInLine = 0;
+                    continue;
+                }
+                var li = document.createElement('li');
+                li.innerHTML = text;
+			    parent.appendChild(li);
+                numInLine++;
 			}
     	});
    		var date = new Date();
